e92b5af20c08413d1607c74eeb9a823d0cbc9acc
admin <admin@mac05.local>
2017-04-07 19:06:01 -0300

    first commit

---
+++ DeboraLanches/Pedido.swift
@@ -0,0 +1,13 @@
+//
+//  Pedido.swift
+//  DeboraLanches
+//
+//  Created by admin on 07/04/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import Foundation
+
+class Pedido: NSObject {
+    
+}
